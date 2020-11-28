d31547609f128428821751978d992dcd08cacdd0
rbisb <rbisb@DESKTOP-MM26GKA>
2020-11-28 22:22:47 +0300

    Stepa suck dick.

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -6,7 +6,7 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Стёпа сосёт большой хуй");
         }
     }
 }
