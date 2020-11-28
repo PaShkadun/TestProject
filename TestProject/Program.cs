dd483f94548b2039f635f96f34dcfb41943637c2
rbisb <rbisb@DESKTOP-MM26GKA>
2020-11-28 22:25:33 +0300

    Edit method "Suck dick".

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -6,7 +6,7 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Стёпа сосёт большой хуй");
+            Stepa.SuckDick();
         }
     }
 }
---
+++ TestProject/Stepa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject
+{
+    class Stepa
+    {
+        public static void SuckDick()
+        {
+            Console.WriteLine("Стёпа сосёт большой хуй");
+        }
+    }
+}
